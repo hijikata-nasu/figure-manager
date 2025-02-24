5decba1549f90574fd856f7bad07cf91ba053385
hijikata-nasu <kaitomikkumiku+deskTop@gmail.com>
2025-02-24 23:15:02 +0900

    データベースの設定を修正
    なんか2つ使ってたので1つにしました。
    テンプレートから作っただけだったので、名前とかいい感じにしました。

--- src/main/kotlin/dev/hijikatanasu/databese/Databases.kt
+++ src/main/kotlin/dev/hijikatanasu/databese/Databases.kt
@@ -17,12 +17,6 @@ import java.sql.DriverManager
 import org.jetbrains.exposed.sql.*
 
 fun Application.configureDatabases() {
-    val database = Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "",
-    )
     val dbConnection: Connection = connectToPostgres(embedded = true)
     val cityService = CityService(dbConnection)
 
@@ -61,41 +55,6 @@ fun Application.configureDatabases() {
             call.respond(HttpStatusCode.OK)
         }
     }
-    val userService = UserService(database)
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
-
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
-    }
 }
 
 /**
@@ -119,16 +78,17 @@ fun Application.configureDatabases() {
  * @return [Connection] that represent connection to the database. Please, don't forget to close this connection when
  * your application shuts down by calling [Connection.close]
  * */
-fun Application.connectToPostgres(embedded: Boolean): Connection {
-    Class.forName("org.postgresql.Driver")
+private fun Application.connectToPostgres(embedded: Boolean): Connection {
     if (embedded) {
+        Class.forName("org.postgresql.Driver")
         log.info("Using embedded H2 database for testing; replace this flag to use postgres")
         return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
-        val url = environment.config.property("postgres.url").getString()
+        Class.forName(environment.config.property("database.driverClassName").getString())
+        val url = environment.config.property("database.url").getString()
         log.info("Connecting to postgres database at $url")
-        val user = environment.config.property("postgres.user").getString()
-        val password = environment.config.property("postgres.password").getString()
+        val user = environment.config.property("database.user").getString()
+        val password = environment.config.property("database.password").getString()
 
         return DriverManager.getConnection(url, user, password)
     }
